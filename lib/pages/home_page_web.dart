051cb20b4f6bdffd72ba9e29e4311ceabc75332e
anatoliyKaragodin <anatoliy.karagodin@gmail.com>
2023-02-27 09:36:45 +0400

    cookies saved(on second auth)

--- lib/pages/home_page_web.dart
+++ lib/pages/home_page_web.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_traning_app_1/utils/library.dart';
-import 'package:webview_cookie_manager/webview_cookie_manager.dart';
 
 import '../main.dart';
 
@@ -11,65 +10,17 @@ class HomePageWeb extends StatefulWidget {
 }
 
 class _HomePageWebState extends State<HomePageWeb> {
-  final cookieManager = WebviewCookieManager();
-
-  /// Get cookies
-  void getCookies(String url) async {
-    final gotCookies = await cookieManager.getCookies(url);
-    for (var item in gotCookies) {
-      print(item);
-    }
-  }
-
-  // /// Set cookies
-  // void setCookies(String url) async {
-  //   cookieManager.setCookies([
-  //     Cookie(cookieName, cookieValue)
-  //       ..domain = domain
-  //       ..expires = DateTime.now().add(Duration(days: 10))
-  //       ..httpOnly = false
-  //   ]);
-  // }
-
   @override
   Widget build(BuildContext context) {
-    WebViewController controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            // Update loading bar.
-          },
-          onPageStarted: (String url) {
-            getCookies(url);
-          },
-          onPageFinished: (String url) {
-            getCookies(url);
-          },
-          onWebResourceError: (WebResourceError error) {},
-          onNavigationRequest: (NavigationRequest request) {
-
-            if (request.url.startsWith(url)) {
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
+    CookieManager cookieManager = CookieManager.instance();
+    InAppWebViewController? webViewController;
+    InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+        crossPlatform: InAppWebViewOptions(
+          useShouldOverrideUrlLoading: true,
+          mediaPlaybackRequiresUserGesture: false,
         ),
-      )
-      ..addJavaScriptChannel(
-        'Toaster',
-        onMessageReceived: (JavaScriptMessage message) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(message.message)),
-          );
-        },
-      )
-      ..loadRequest(Uri.parse(url));
-    controller.canGoBack();
-
-    /// Get cookies
-    getCookies(url);
+        android: AndroidInAppWebViewOptions(thirdPartyCookiesEnabled: true));
+    Uri uri = Uri.parse(url);
 
     return Scaffold(
         body:
@@ -77,9 +28,42 @@ class _HomePageWebState extends State<HomePageWeb> {
             /// Webview widget
             WillPopScope(
                 onWillPop: () async {
-                  controller.goBack();
+                  webViewController?.goBack();
                   return false;
                 },
-                child: WebViewWidget(controller: controller)));
+                child: InAppWebView(
+                  onWebViewCreated: (controller) async {
+                    final prefs = await SharedPreferences.getInstance();
+                    var cookiesNames = prefs.getKeys();
+                    for (var element in cookiesNames) {
+                      var cookieValue = prefs.getString(element);
+                      cookieManager.setCookie(
+                          url: uri, name: element, value: cookieValue!);
+                      webViewController = controller;
+                    }
+                  },
+                  initialOptions: options,
+                  onLoadStart:
+                      (InAppWebViewController controller, Uri? url) async {
+                    // final prefs = await SharedPreferences.getInstance();
+                    // var cookiesNames = prefs.getKeys();
+                    // for (var element in cookiesNames) {
+                    //   var cookieValue = prefs.getString(element);
+                    //   cookieManager.setCookie(
+                    //       url: url!, name: element, value: cookieValue!);
+                    // }
+                  },
+                  onLoadStop:
+                      (InAppWebViewController controller, Uri? url) async {
+                    final prefs = await SharedPreferences.getInstance();
+                    List<Cookie> cookies =
+                        await cookieManager.getCookies(url: url!);
+                    cookies.forEach((cookie) async {
+                      print(cookie.name + " " + cookie.value);
+                      await prefs.setString(cookie.name, cookie.value);
+                    });
+                  },
+                  initialUrlRequest: URLRequest(url: uri),
+                )));
   }
 }
